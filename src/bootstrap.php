a6827dc56e1cb02d9cc2887169c7e20c64d743db
Jonáš Vysocký <jonas.vysocky@gmail.com>
2024-11-30 15:05:58 +0100

    Add Guzzle and Dotenv

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,8 +3,21 @@
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Mapping\UnderscoreNamingStrategy;
 use Doctrine\ORM\ORMSetup;
+use Dotenv\Dotenv;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+$rootFolder = __DIR__.'/../';
+
+$envNames = ['.env', '.env.local'];
+foreach ($envNames as $key => $env) {
+    if (false === file_exists($rootFolder . $env)) {
+        unset($envNames[$key]);
+    }
+}
+
+$dotenv = Dotenv::createImmutable($rootFolder, $envNames, false);
+$dotenv->load();
 
 $config = ORMSetup::createAttributeMetadataConfiguration([__DIR__], true);
 $config->setNamingStrategy(new UnderscoreNamingStrategy());
