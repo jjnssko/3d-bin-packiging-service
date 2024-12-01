1995d4399ab36bb127a23e47debc32dc3458eea6
Jonáš Vysocký <jonas.vysocky@gmail.com>
2024-12-01 20:43:32 +0100

    Update product normalization

--- src/Service/ProductService.php
+++ src/Service/ProductService.php
@@ -4,15 +4,30 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use App\Entity\PackagingResult;
 use App\Normalizer\ProductNormalizer;
 
 class ProductService
 {
+    /**
+     * @param array<int, array<string, int|float>> $requestedProducts
+     * @return array<int, array<string, int|float>>
+     */
     public function getProductsForPacking(array $requestedProducts): array
     {
-        return array_map(
-            fn($product) => ProductNormalizer::normalizeForClient($product),
-            $requestedProducts
-        );
+        $normalizedProducts = [];
+
+        foreach ($requestedProducts as $product) {
+            $normalized = ProductNormalizer::normalizeForClient($product);
+            $hash = PackagingResult::generateInputHash($product);
+
+            if (isset($normalizedProducts[$hash])) {
+                $normalizedProducts[$hash]['q']++;
+            } else {
+                $normalizedProducts[$hash] = $normalized;
+            }
+        }
+
+        return array_values($normalizedProducts);
     }
 }
